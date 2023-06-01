666ed448510d5ecce5aae2a6b237181fe7c4e649
Dror Livne <livme.dror@gmail.com>
2023-06-01 12:11:38 +0300

    union-based repr

--- query.cpp
+++ query.cpp
@@ -2,17 +2,6 @@
 #include <sstream>
 #include <set>
 
-enum Kind {
-  TVAR = 0,
-  TINT,
-  TFLOAT,
-  TSTRING,
-};
-
-struct Value {
-  virtual std::string label() const = 0;
-  virtual Kind kind() const = 0;
-};
 #include "flat/set"
 #include "flat/span"
 
@@ -28,130 +17,194 @@ public:
   operator std::string() const { return s.str(); }
 };
 
-struct Int : Value {
-  int i;
-  Int(int i): i(i) {}
-  std::string label() const override { return std::to_string(i); }
-  Kind kind() const override { return TINT; }
+enum Kind {
+  TVAR = 0,
+  TINT,
+  TFLOAT,
+  TSTRING,
 };
 
-struct String : Value {
-  std::string s;
-  std::string label() const override { return format{} << "\"" << s << "\""; }
-  Kind kind() const override { return TSTRING; }
+struct Var;
+union Value {
+  long i;
+  float f;
+  const std::string *s;
+  const Var *v;
+  Value(int i): i(i) {}
+  Value(float f): f(f) {}
+  Value(const std::string& s): s(&s) {}
+  Value(const Var &v): v(&v) {}
+  void format(std::ostream& os, Kind k) const;
+  static bool cmp(Value l, Value r, Kind k);
 };
 
-struct Var : Value {
+template<class T>
+struct kind_of {};
+
+template<> struct kind_of<int> : std::integral_constant<Kind, TINT> {};
+template<> struct kind_of<float> : std::integral_constant<Kind, TFLOAT> {};
+template<> struct kind_of<std::string> : std::integral_constant<Kind, TSTRING> {};
+template<size_t N> struct kind_of<const char(&)[N]> : std::integral_constant<Kind, TSTRING> {};
+template<> struct kind_of<Var> : std::integral_constant<Kind, TVAR> {};
   
-  std::string name;
-  Var(const std::string& name): name(name) {}
-  Kind kind() const override { return TVAR; }
-  std::string label() const override
+struct EDB {
+  std::set<std::string> intern;
+
+  Value of(int i) { return Value(i); }
+  Value of(float f) { return Value(f); }
+  Value of(const char *s) { return of(std::string(s)); }
+  Value of(const std::string& s)
   {
-    format f;
-    f << "?" << name;
-    if (val)
-      f << "=" << val->label();
-    return f;
+    auto itb = intern.emplace(s);
+    return Value(*itb.first);
   }
+};
+
+struct Var {
+  Kind kind;
+  std::string name;
+  Var(Kind kind, const std::string& name): kind(kind), name(name) {}
   bool operator<(const Var& o) const { return name < o.name; }
 
-  mutable unsigned epoch = 0;
   mutable const Value *val = nullptr;
+  bool unify(const Value *v) const
+  {
+    if (val)
+      return Value::cmp(*val, *v, kind);
+    val = v;
+    return true;
+  }
 };
 
+void Value::format(std::ostream& os, Kind k) const
+{
+  switch (k) {
+  case TINT: os << i; break;
+  case TFLOAT: os << f; break;
+  case TSTRING: os << "\"" << *s << "\""; break;
+  case TVAR:
+    os << "?" << v->name;
+    if (v->val) {
+      os << "=";
+      v->val->format(os, v->kind);
+    }
+    break;
+  }
+}
+
+bool Value::cmp(Value l, Value r, Kind k) {
+  switch (k) {
+  case TINT: return l.i == r.i;
+  case TFLOAT: return l.f == r.f;
+  case TSTRING: return l.s == r.s; // Strings are interned!
+  case TVAR:
+    assert(false);
+  }
+}
+
 struct Tuple {
-  Tuple(std::initializer_list<const Value*> l): vals(l) {}
-  std::vector<const Value*> vals;
-  friend std::ostream& operator<<(std::ostream& os, const Tuple& t)
+  Tuple(std::initializer_list<Value> l): vals(l) {}
+  std::vector<Value> vals;
+  void format(std::ostream& os, flat::span<Kind> kind) const
   {
     int i=0;
-    for (auto v : t.vals) {
-      if (i++) os << ", ";
-      os << v->label();
+    for (auto const& v : vals) {
+      if (i) os << ", ";
+      v.format(os, kind[i]);
+      i++;
     }
+  }
+};
+
+struct Relation {
+  EDB& edb;
+  std::string name;
+  std::vector<Kind> type;
+  Relation(EDB& edb, const std::string& name, const std::vector<Kind>& type): edb(edb), name(name), type(type) {}
+
+  flat::set<Tuple> all;
+  friend std::ostream& operator<<(std::ostream& os, const Relation& r)
+  {
+    os << "{";
+    for (auto const& t : r.all) {
+      os << "\n  " << r.name << "(";
+      t.format(os, r.type);
+      os  << ")";
+    }
+    os <<"\n}";
     return os;
   }
+
+
+  //void insert(Tuple&& t) { all.emplace(t); }
 
   // unify([1, 2, 3], [1, 2, 3]) -> true
   // unify([1, 2, 3], [1, x, y]) -> true
   // unify([1, 2, 3], [1, x, 4]) -> false
   
   // unify([1, 2, 3], [1, x, x]) -> false
-  bool unify(const Tuple& sel) const {
-    static unsigned epoch = 0;
-
-    epoch++;
-    assert(vals.size() == sel.vals.size());
-    for (int i=0; i<vals.size(); i++) {
-      const Value *l = vals[i];
-      const Value *rv = sel.vals[i];
-      if (l == rv)              // FIXME: compare?
-        continue;
-
-      const Var *v = dynamic_cast<const Var*>(rv);
-      if (!v)
+  bool unify(const Tuple& t, const Tuple& sel, flat::span<Kind> selkind) const {
+    assert(t.vals.size() == sel.vals.size());
+    for (int i=0; i<t.vals.size(); i++) {
+      Kind kind = selkind[i];
+      if (TVAR == kind) {
+        const Var *v = sel.vals[i].v;
+        if (!v->unify(&t.vals[i])) {
+          return false;
+        }
+      } else if (!Value::cmp(sel.vals[i], t.vals[i], kind))
         return false;
-
-      if (v->epoch != epoch) {
-        v->epoch = epoch;
-        v->val = l;
-      } else if (v->val != l) {
-        return false;
-      }
     }
     return true;
   }
-  bool operator<(const Tuple& o) const { return vals < o.vals; }
-};
 
-struct Relation {
-  Relation(const std::string& name, const std::vector<Kind>& type): name(name), type(type) {}
-  std::string name;
-  std::vector<Kind> type;
-
-  flat::set<Tuple> all;
-  friend std::ostream& operator<<(std::ostream& os, const Relation& r)
+  template<typename... Args>
+  std::pair<Tuple, std::vector<Kind>>
+  tuplify(Args&&... args)
   {
-    os << "{";
-    for (auto const& t : r.all)
-      os << "\n  " << r.name << "(" << t << ")";
-    os <<"\n}";
-    return os;
+    std::vector<Kind> kinds({(kind_of<Args>::value)...});
+    assert(kinds == type);
+    Tuple vals({edb.of(args)...});
+    return {std::move(vals), std::move(kinds)};
   }
-
-
-  void insert(Tuple&& t) { all.emplace(t); }
-  int select(const Tuple& sel)
-  {
-    int res = 0;
-    std::cout << name << ".select(" << sel << "): ";
-    for (auto const& t : all) {
-      if (t.unify(sel)) {
-        std::cout << sel << "\n";
-        res++;
-      }
-    }
-    std::cout << res << "\n";
-    return res;
-  }
+  
+  // int select(const Tuple& sel)
+  // {
+  //   int res = 0;
+  //   std::cout << name << ".select(" << sel << "): ";
+  //   for (auto const& t : all) {
+  //     if (t.unify(sel)) {
+  //       std::cout << sel << "\n";
+  //       res++;
+  //     }
+  //   }
+  //   std::cout << res << "\n";
+  //   return res;
+  // }
 };
 
 int main()
 {
-  Int i[] = {Int(0), Int(1), Int(2), Int(3)};
+  EDB edb;
+  Relation R(edb, "R", {TINT, TINT, TINT});
+  
+  auto vk = R.tuplify(1, 2, 3);
+  vk.first.format(std::cout, vk.second);
+  //R.insert(vk.first);
+  // R.insert(Tuple({&i[1], &i[2], &i[3]}));
+  // R.insert(Tuple({&i[1], &i[2], &i[3]}));
+  // R.insert(Tuple({&i[1], &i[1], &i[3]}));
+  //std::cout << R << "\n"; 
 
-  Relation R("R", {TINT, TINT, TINT});
-  R.insert(Tuple({&i[1], &i[2], &i[3]}));
-  R.insert(Tuple({&i[1], &i[2], &i[3]}));
-  R.insert(Tuple({&i[1], &i[1], &i[3]}));
-  std::cout << R << "\n"; 
+  Relation S(edb, "S", {TSTRING, TINT, TSTRING});
+  auto vk2 = S.tuplify("Hello", 2, "Hello");
+  vk2.first.format(std::cout, vk2.second);
+  
+  // Var x("x");
+  // Var y("y");
 
-  Var x("x");
-  Var y("y");
-
-  R.select(Tuple({&i[1], &i[2], &i[3]}));
-  R.select(Tuple({&i[1], &x, &y}));
-  R.select(Tuple({&i[1], &x, &i[0]}));
-  R.select(Tuple({&x, &x, &i[3]}));
+  // R.select(Tuple({&i[1], &i[2], &i[3]}));
+  // R.select(Tuple({&i[1], &x, &y}));
+  // R.select(Tuple({&i[1], &x, &i[0]}));
+  // R.select(Tuple({&x, &x, &i[3]}));
 }
