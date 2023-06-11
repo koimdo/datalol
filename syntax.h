3373e53208accafe7837e093dda6064163c0a976
Dror Livne <livme.dror@gmail.com>
2023-06-11 15:51:30 +0300

    small syntax changes

--- syntax.h
+++ syntax.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <iostream>
 #include <memory>
+#include "flat/span"
 
 struct IPrint {
   virtual void print(std::ostream&) const = 0;
@@ -14,18 +15,21 @@ struct IPrint {
 class Rule : public IPrint {
 public:
   Rule(Rule&&) = default;
-  struct Head : IPrint { virtual bool eval_head(Rule*) = 0; };
-  struct Body : IPrint { virtual bool eval_body(Rule*) = 0; };
+  struct Head : IPrint { virtual void eval_head(Rule&) = 0; };
+  struct Body : IPrint { virtual void eval_body(Rule&) = 0; };
 
   using uhead = std::unique_ptr<Head>;
   using ubody = std::unique_ptr<Body>;
 
-  uhead head;
   friend Rule operator<<(uhead head, ubody b);
   friend Rule operator& (Rule&& rule, Rule::ubody e);
-  void append(ubody b);
+
+  Head *get_head() { return head.get(); }
+  flat::span<Body*> get_body() { return { reinterpret_cast<Body**>(body.data()), body.size() }; }
 private:
+  void append(ubody b);
   void print(std::ostream& os) const override;
+  uhead head;
   std::vector<ubody> body;
   explicit Rule(uhead head);
 };
@@ -34,9 +38,15 @@ Rule operator<<(Rule::uhead head, Rule::ubody e);
 Rule operator& (Rule&& rule, Rule::ubody e);
 
 class Query : public IPrint {
+protected:
   std::vector<Rule> rules;
   void print(std::ostream& os) const override;
 public:
+  template<std::size_t N>
+  Query(Rule(&&rs)[N])
+  : rules(std::make_move_iterator(std::begin(rs)), std::make_move_iterator(std::end(rs)))
+{}
+  Query(Query&&) = default;
   Query(Rule&&);
   Query operator,(Rule&& r) &&;
 };
