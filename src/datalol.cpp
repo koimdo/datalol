636d2d4d3dc9b4e084bf420ed6a4f4cb87eb411a
Dror Livne <livme.dror@gmail.com>
2025-01-14 13:29:50 +0200

    unify syntax.cpp and query.cpp

--- src/datalol.cpp
+++ src/datalol.cpp
@@ -1,7 +1,181 @@
+#include <cassert>
+#include <algorithm>
+#include <cstring>
+#include <limits>
+
+#include <flat/pnr_utils.h>
 #include <datalol/syntax.h>
 #include <datalol/query.h>
 #include <datalol/debug.h>
-#include <limits>
+
+Query *Query::current = nullptr;
+Var_::vars_t *Var_::current_vars = nullptr;
+
+Json::Value IPrint::to_json() const {
+  std::ostringstream os;
+  print(os);
+  return Json::Value(os.str());
+}
+
+bool type_id_t::operator==(type_id_t o) const { return !strcmp(type, o.type); }
+bool type_id_t::operator!=(type_id_t o) const { return strcmp(type, o.type); }
+bool type_id_t::operator<(type_id_t o) const { return strcmp(type, o.type) < 0; }
+
+
+std::string type_id_t::type_name() const
+{
+  auto left = strchr(type, '=')+2;
+  auto right = strrchr(left, ']');
+  return std::string(left, right-left);
+}
+
+std::string ident::get_name() const
+{
+  if (name)
+    return name;
+  return std::string();
+}
+
+Rule::cursor::cursor(uhead&& hh, ubody&& bb)
+{
+  auto q = Query::current;
+
+  r = q->start_rule();
+  q->add_elem(hh);
+  q->add_elem(bb);
+}
+
+Rule::cursor::~cursor()
+{
+  Query::current->end_rule(r);
+}
+
+Rule::cursor operator<<(Rule::uhead&& h, Rule::ubody&& b)
+{
+  return Rule::cursor(std::move(h), std::move(b));
+}
+
+Rule::cursor& Rule::cursor::operator&(Rule::ubody&& b)
+{
+  Query::current->add_elem(b);
+  return *this;
+}
+
+std::ostream& operator<<(std::ostream& os, const Var_::Impl& impl)
+{
+  os << "?" << impl.id.get_name() << "[" << impl.id.type_name() << "]";
+  return os;
+}
+
+void Query::add_elem(Rule::uelem me)
+{
+  me->idx = elems.size()-rules.back().head;
+  elems.push_back(me);
+}
+
+Rule *Query::start_rule()
+{
+  Rule r;
+  r.head = elems.size();
+  rules.push_back(r);
+  return &rules.back();
+}
+
+void Query::end_rule(Rule *r)
+{
+  assert(&rules.back() == r && !r->last);
+  r->last = elems.size();
+}
+
+Query::Query(debug_info *dbg, const char *name)
+  : pool(name)
+  , dbg(dbg)
+{
+  current_query.set(&current, this);
+}
+
+void Query::iter::operator++()
+{
+  q->configure();
+  q->print(std::cout);          // TODO: remove printf
+  std::cout << "\n";
+  q->run();
+  q = nullptr;
+}
+
+void Query::print_rule(std::ostream& os, const Rule& r) const
+{
+  auto print_with_vars = [this, &os](const Rule::Elem& e) {
+    os << "[";
+    print_vars(os, e.meta);
+    os << "]";
+    e.print(os);
+  };
+  auto const& h = elems[r.head];
+  print_with_vars(*h);
+  os << " << ";
+  int count = 0;
+  for (int j = r.head+1; j < r.last; j++) {
+    os << (count++ ? " & " : "") << (r.seminaive_current == j-r.head ? "^" : "");
+    auto const& b = elems[j];
+    print_with_vars(*b);
+  }
+}
+
+void Query::print(std::ostream& os) const
+{
+  os << "Query: {";
+  size_t i=0;
+  for (auto const& r : rules) {
+    if (i) os << "\n";
+    print_rule(os, r);
+    os << ";";
+    i++;
+  }
+  os <<"}";
+}
+
+void Query::print_vars(std::ostream& os, const Rule::elem_meta& vs) const
+{
+  int i=0;
+  int out = 0;
+  for (auto const& v : vars) {
+    bool is_pos = vs.positive.test(i);
+    bool is_neg = vs.negative.test(i);
+    i++;
+    if (!is_pos && !is_neg)
+      continue;
+    os << (out?", ":"") << (is_pos ? "+":"") << (is_neg ? "-":"") << v.impl->id.get_name();
+    out++;
+  }
+}
+
+Var_::Impl::Impl(ident id)
+  : id(id)
+{}
+
+void Var_::register_var(const Var_* v)
+{
+  assert(current_vars);
+  current_vars->set(v->impl->nvar);
+}
+
+Rule::Elem::Elem(const elem_meta& m)
+  : meta(m)
+{
+}
+
+thunk_base::thunk_base(const char *desc)
+  : desc(desc)
+  , vars(*Var_::current_vars)
+{
+  Var_::current_vars = nullptr;
+}
+
+std::ostream& operator<<(std::ostream& os, const thunk_base& t)
+{
+  return os << "THUNK(" << t.desc << ")";
+}
 
 bool Query::cmp::operator()(Collection_base *l, Collection_base *r) const
 {
@@ -185,5 +359,3 @@ void Query::run()
   DEBUG_PROBE(BREAK_END);
   dbg->tripcount++;
 }
-
-
